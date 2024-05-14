9da82212cd75b6c85f76815e3b04eea3c65d398e
sabin-b <sabinb1998sabinb@gmail.com>
2024-05-14 06:47:30 +0530

    dark mode localstorage error fixed

--- components/darkmodetoggle/DarkModeToggleBtn.jsx
+++ components/darkmodetoggle/DarkModeToggleBtn.jsx
@@ -1,13 +1,25 @@
 'use client';
 import useDarkMode from '@/darkmodecontext/useDarkMode';
+import { useEffect, useState } from 'react';
 import { FiSun } from 'react-icons/fi';
 import { IoMoonSharp } from 'react-icons/io5';
 function DarkModeToggleBtn() {
   const { isDarkMode, toggleDarkMode } = useDarkMode();
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <button onClick={toggleDarkMode} className='inline'>
-      {isDarkMode ? <IoMoonSharp /> : <FiSun />}
-    </button>
+    <>
+      {isClient && (
+        <button onClick={toggleDarkMode} className='inline'>
+          {isDarkMode ? <IoMoonSharp /> : <FiSun />}
+        </button>
+      )}
+    </>
   );
 }
 
--- darkmodecontext/DarkModeProvider.jsx
+++ darkmodecontext/DarkModeProvider.jsx
@@ -1,27 +1,31 @@
-"use client";
-import { createContext, useEffect, useState } from "react";
+'use client';
+import { createContext, useEffect, useState } from 'react';
 
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
   const [isDarkMode, setDarkMode] = useState(function () {
-    const status = localStorage.getItem("DarkMode");
-    return status
-      ? JSON.parse(status)
-      : window.matchMedia("(prefers-color-scheme: dark)").matches;
+    try {
+      const status = localStorage.getItem('DarkMode');
+      return status
+        ? JSON.parse(status)
+        : window.matchMedia('(prefers-color-scheme: dark)').matches;
+    } catch (error) {
+      return false;
+    }
   });
 
   // set that value in localstorage
   useEffect(() => {
-    localStorage.setItem("DarkMode", JSON.stringify(isDarkMode));
+    localStorage.setItem('DarkMode', JSON.stringify(isDarkMode));
   }, [isDarkMode]);
 
   // change darkMode & lightMode
   useEffect(() => {
     if (isDarkMode) {
-      document.documentElement.classList.add("dark");
+      document.documentElement.classList.add('dark');
     } else {
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.remove('dark');
     }
   }, [isDarkMode]);
 
