d6e65c02a86a6c20ed1709cae68eb11802bff732
Giridhar M A <m.a.giridhar21@gmail.com>
2024-06-12 14:31:58 +0530

    Created products page

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -8,7 +8,7 @@ export async function GET(request) {
     const search = searchParams.get("search");
     const status =
       searchParams.get("status") !== undefined &&
-      searchParams.get("status") !== "select" &&
+      searchParams.get("status") !== "Status" &&
       searchParams.get("status") !== ""
         ? searchParams.get("status")
         : undefined;
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,14 +1,14 @@
-import { Dashboardfilter } from '@/components/dashboard/dashboardfilter';
-import { Dashboardpagewrapper } from '@/components/dashboard/dashboardpagewrapper';
-import { LoadingSpinner } from '@/components/loadingspinner/LoadingSpinner';
-import { Suspense } from 'react';
+import { Dashboardfilter } from "@/components/dashboard/dashboardfilter";
+import { Dashboardpagewrapper } from "@/components/dashboard/dashboardpagewrapper";
+import { LoadingSpinner } from "@/components/loadingspinner/LoadingSpinner";
+import { Suspense } from "react";
 
 export default async function DashboardPage({ searchParams }) {
-  const currentfilter = (await searchParams.last) ?? '7';
+  const currentfilter = (await searchParams.last) ?? "7";
   return (
-    <div className='flex flex-col gap-6 pb-12'>
-      <h2 className='text-2xl mb-4 font-medium'>Dashboard Overview</h2>
-      <div className='flex gap-x-1 justify-end'>
+    <div className="flex flex-col gap-6 pb-12">
+      <h2 className="text-2xl mb-4 font-medium">Dashboard Overview</h2>
+      <div className="flex gap-x-1 justify-end">
         <Dashboardfilter />
       </div>
       <Suspense key={currentfilter} fallback={<LoadingSpinner />}>
